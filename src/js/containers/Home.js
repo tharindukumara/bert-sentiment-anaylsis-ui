ea87f1ba16645be6aba5013d14e152c3428fc044
Tharindu Kumara <zonik.hatkumara@gmail.com>
2020-06-08 22:02:42 +0530

    Changes

--- src/js/containers/Home.js
+++ src/js/containers/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Anchor, Box, Image, Paragraph, Text, TextArea } from 'grommet';
+import { Anchor, Button, Box, Image, Paragraph, Text, TextArea } from 'grommet';
 import { Github, Slack, Twitter } from 'grommet-icons';
 import { AcceptIcon } from '@fluentui/react-icons-northstar';
-import {  Avatar, Chat, Divider, Provider, themes } from '@fluentui/react-northstar'
+import { Avatar, Chat, Divider, Provider, themes } from '@fluentui/react-northstar'
 import Header from '../components/Header'
 import Section from '../components/Section';
+import ChatBox from '../components/ChatBox';
 
 class Home extends React.Component {
     constructor(props) {
@@ -16,111 +17,111 @@ class Home extends React.Component {
         const janeAvatar = {
             image: 'public/images/avatar/small/ade.jpg',
             status: {
-              color: 'green',
-              icon: <AcceptIcon />,
+                color: 'green',
+                icon: <AcceptIcon />,
             },
-          };
-          const items = [
+        };
+        const items = [
             {
-              message: (
-                <Chat.Message content="Hello" author="John Doe" timestamp="Yesterday, 10:15 PM" mine />
-              ),
-              contentPosition: 'end',
-              attached: 'top',
-              key: 'message-id-1',
-            },
-            {
-              message: (
-                <Chat.Message content="I'm back!" author="John Doe" timestamp="Yesterday, 10:15 PM" mine />
-              ),
-              contentPosition: 'end',
-              attached: true,
-              key: 'message-id-2',
+                message: (
+                    <Chat.Message content="Hello" author="John Doe" timestamp="Yesterday, 10:15 PM" mine />
+                ),
+                contentPosition: 'end',
+                attached: 'top',
+                key: 'message-id-1',
             },
             {
-              message: (
-                <Chat.Message
-                  content={{
-                    content: (
-                      <div>
-                        What do you think about <a href="#">www.goodFood.com</a>?
-                      </div>
-                    ),
-                  }}
-                  author="John Doe"
-                  timestamp="Yesterday, 10:15 PM"
-                  mine
-                />
-              ),
-              contentPosition: 'end',
-              attached: 'bottom',
-              key: 'message-id-3',
+                message: (
+                    <Chat.Message content="I'm back!" author="John Doe" timestamp="Yesterday, 10:15 PM" mine />
+                ),
+                contentPosition: 'end',
+                attached: true,
+                key: 'message-id-2',
             },
             {
-              gutter: <Avatar {...janeAvatar} />,
-              message: <Chat.Message content="Hi" author="Jane Doe" timestamp="Yesterday, 10:15 PM" />,
-              attached: 'top',
-              key: 'message-id-4',
+                message: (
+                    <Chat.Message
+                        content={{
+                            content: (
+                                <div>
+                                    What do you think about <a href="#">www.goodFood.com</a>?
+                                </div>
+                            ),
+                        }}
+                        author="John Doe"
+                        timestamp="Yesterday, 10:15 PM"
+                        mine
+                    />
+                ),
+                contentPosition: 'end',
+                attached: 'bottom',
+                key: 'message-id-3',
             },
             {
-              gutter: <Avatar {...janeAvatar} />,
-              message: (
-                <Chat.Message content="Looks good!" author="Jane Doe" timestamp="Yesterday, 10:15 PM" />
-              ),
-              attached: true,
-              key: 'message-id-5',
+                gutter: <Avatar {...janeAvatar} />,
+                message: <Chat.Message content="Hi" author="Jane Doe" timestamp="Yesterday, 10:15 PM" />,
+                attached: 'top',
+                key: 'message-id-4',
             },
             {
-              gutter: <Avatar {...janeAvatar} />,
-              message: (
-                <Chat.Message
-                  content={
-                    <div>
-                      I also like <a href="#">www.goodFood2.com</a>.
+                gutter: <Avatar {...janeAvatar} />,
+                message: (
+                    <Chat.Message content="Looks good!" author="Jane Doe" timestamp="Yesterday, 10:15 PM" />
+                ),
+                attached: true,
+                key: 'message-id-5',
+            },
+            {
+                gutter: <Avatar {...janeAvatar} />,
+                message: (
+                    <Chat.Message
+                        content={
+                            <div>
+                                I also like <a href="#">www.goodFood2.com</a>.
                     </div>
-                  }
-                  author="Jane Doe"
-                  timestamp="Yesterday, 10:15 PM"
-                />
-              ),
-              attached: 'bottom',
-              key: 'message-id-6',
+                        }
+                        author="Jane Doe"
+                        timestamp="Yesterday, 10:15 PM"
+                    />
+                ),
+                attached: 'bottom',
+                key: 'message-id-6',
             },
             {
-              message: (
-                <Chat.Message
-                  content="Would you like to grab lunch there?"
-                  author="John Doe"
-                  timestamp="Yesterday, 10:16 PM"
-                  mine
-                />
-              ),
-              contentPosition: 'end',
-              key: 'message-id-7',
+                message: (
+                    <Chat.Message
+                        content="Would you like to grab lunch there?"
+                        author="John Doe"
+                        timestamp="Yesterday, 10:16 PM"
+                        mine
+                    />
+                ),
+                contentPosition: 'end',
+                key: 'message-id-7',
             },
             {
-              gutter: <Avatar {...janeAvatar} />,
-              message: (
-                <Chat.Message
-                  content="Sure! Let's try it."
-                  author="Jane Doe"
-                  timestamp="Yesterday, 10:15 PM"
-                />
-              ),
-              key: 'message-id-8',
+                gutter: <Avatar {...janeAvatar} />,
+                message: (
+                    <Chat.Message
+                        content="Sure! Let's try it."
+                        author="Jane Doe"
+                        timestamp="Yesterday, 10:15 PM"
+                    />
+                ),
+                key: 'message-id-8',
             },
             {
-              children: <Divider content="Today" color="brand" important />,
-              key: 'message-id-9',
+                children: <Divider content="Today" color="brand" important />,
+                key: 'message-id-9',
             },
             {
-              message: (
-                <Chat.Message content="Ok, let's go." author="John Doe" timestamp="Today, 11:15 PM" mine />
-              ),
-              contentPosition: 'end',
-              key: 'message-id-10',
+                message: (
+                    <Chat.Message content="Ok, let's go." author="John Doe" timestamp="Today, 11:15 PM" mine />
+                ),
+                contentPosition: 'end',
+                key: 'message-id-10',
             },
-          ];
+        ];
 
 
 
@@ -135,6 +136,18 @@ class Home extends React.Component {
                                 summary="build responsive and accessible mobile-first projects for the
                         web with an easy to use component library"
                             />
+                            <Box align="justify" width="100%" direction="row-responsive" gap="small">
+                                <Button hoverIndicator="light-1" onClick={() => { }} >
+                                    <Box pad={{ horizontal: "large", vertical: "small" }} direction="row" align="center" gap="small" background="neutral-2">
+                                        <Text>Add</Text>
+                                    </Box>
+                                </Button>
+                                <Button hoverIndicator="light-1" onClick={() => { }} >
+                                    <Box pad={{ horizontal: "large", vertical: "small" }} direction="row" align="center" gap="small" background="neutral-2">
+                                        <Text>Add</Text>
+                                    </Box>
+                                </Button>
+                            </Box>
                         </Box>
                         <Box width="xlarge" background="light-2" pad="small" elevation="medium">
                             <Box
@@ -145,18 +158,13 @@ class Home extends React.Component {
                                 margin="xxsmall"
                                 gap="small"
                                 justify="center"
-                                
+
                             >
-                                <Box overflow={{ horizontal: "hidden", vertical: "auto" }} >
-                                <Provider theme={themes.teams}>
-                                    <Chat items={items} />
-                                </Provider>
-                                    
-                                </Box>
-                              
+                                <ChatBox />
+
                             </Box>
                             <Box align="center" pad="xsmall">
-                                <TextArea value="Wradasd" resize={false}/>
+                                <TextArea value="Wradasd" resize={false} />
                             </Box>
                         </Box>
                     </Box>
